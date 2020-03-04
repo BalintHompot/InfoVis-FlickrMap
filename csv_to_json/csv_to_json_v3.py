c6e44a30c0278927d0e91925fb10b5483db6a108
mve88 <43540666+mve88@users.noreply.github.com>
2020-03-04 13:50:11 +0100

    Add files via upload

--- csv_to_json/csv_to_json_v3.py
+++ csv_to_json/csv_to_json_v3.py
@@ -7,34 +7,51 @@ from heapq import nlargest
 arg=sys.argv
 f1 = open("image_info_geo_2.csv",'r')
 next(f1)
+
+ts = "timesteps"
+cd = "country_data"
+t5 = "most_popular_5"
+at = "all_tags"
+bu = "buckets_temp"
+buck = "buckets"
 d={}
+d["allTags"] = {}
+d[ts] = {}
+l = []
+
 for row in f1:
 	r = row.strip().split(";")
 	tags = r[10].split(" ")
-	if r[1] not in d:
-		d[r[1]] = {}
-	if r[6] not in d[r[1]]:
-		d[r[1]][r[6]] = {}
-		d[r[1]][r[6]]["top5"] = {}
-		d[r[1]][r[6]]["all"] = {}
+	if r[1] not in d[ts]:
+		d[ts][r[1]] = {}
+		d[ts][r[1]][cd] = {}
+		d[ts][r[1]][cd][bu] = {}
+		d[ts][r[1]][cd][buck] = {}
+	if r[6] not in d[ts][r[1]][cd][bu]:
+		d[ts][r[1]][cd][bu][r[6]] = {}
+		d[ts][r[1]][cd][bu][r[6]][t5] = {}
+		d[ts][r[1]][cd][bu][r[6]][at] = {}
 	for each in tags:
-		if each in d[r[1]][r[6]]["all"]:
-			d[r[1]][r[6]]["all"][each] += 1
+		l.append(each)
+		if each in d[ts][r[1]][cd][bu][r[6]][at]:
+			d[ts][r[1]][cd][bu][r[6]][at][each] += 1
 		else:
-			d[r[1]][r[6]]["all"][each] = 1
-
+			d[ts][r[1]][cd][bu][r[6]][at][each] = 1
+ 
+s = set(l)
+l = list(l)
+d["allTags"] = l
 #print(max(d[r[1]][r[6]], key=d[r[1]][r[6]].get))
-for k in d:
-	for k2 in d[k]:
-		five_largest = nlargest(5, d[k][k2]["all"], key=d[k][k2]["all"].get)
+for k in d[ts]:
+	bl = []
+	bl.append(d[ts][k][cd][bu])
+	d[ts][k][cd][buck] = bl
+	for k2 in d[ts][k][cd][bu]:
+		five_largest = nlargest(5, d[ts][k][cd][bu][k2][at], key=d[ts][k][cd][bu][k2][at].get)
 		for each in five_largest:
-			d[k][k2]["top5"][each] = d[k][k2]["all"].get(each)
-#			print((d[r[1]][r[6]]["all"][each], key=d[r[1]][r[6]]["all"][each]), d[r[1]][r[6]]["all"].get(each,""))
-#			print(each,d[r[1]][r[6]]["all"].get(each,""))
-#		print(five_largest)
-#five_largest = nlargest(5, d["DZA"]["2007_12"], key=d["DZA"]["2007_12"].get)
-#print(five_largest)
+			d[ts][k][cd][bu][k2][t5][each] = d[ts][k][cd][bu][k2][at].get(each)
+	del(d[ts][k][cd][bu])
 
-with open('result3_fullset.json', 'w') as fp:
+with open('result4_fullset.json', 'w') as fp:
     json.dump(d, fp, indent=4)
 
